261c9ff1be4486ada6a123e3168836fdf11834f6
bm1611 <bm1611@naver.com>
2026-05-13 11:35:14 +0900

    day19 fastapi REST

---
+++ day19/controller.py
@@ -0,0 +1,39 @@
+
+
+# [1] 라우터 : 특정 도메인(주소)만 묶어주는 역할
+from fastapi import APIRouter
+
+# APIRouter( prefix='/공통도메인)
+# vs #RequestMapping("공통도메인")
+router = APIRouter( prefix = "/api" )
+
+# 스프링과 달리 등록해줘야함
+# [3] 서비스 불러오기
+# private final BoardService boardService;
+from service import item_service
+
+# [2] REST API 정의
+# (1) GET
+@router.get("/item")
+async def item( id: int ):
+    return item_service.item( id )
+
+# (2) GET
+@router.get("/items")
+async def items():
+    return item_service.items()
+
+# (3) POST
+@router.post("/save")
+async def save(item: dict):
+    return item_service.save( item )
+
+# (4) PUT
+@router.put("/update")
+async def update( item: dict):
+    return item_service.update( item )
+
+# (5) DELETE
+@router.delete("/delete")
+async def delete( id: int ):
+    return item_service.delete( id )
